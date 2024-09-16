1caa9ff2992879c120cfdb69fb66f3ad87d9128c
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-09-16 18:06:38 -0400

    header

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,5 @@ export default {
 
 <style lang="scss">
  @use "./styles/general.scss" as *;
+ @use "./styles/partials/variables.scss" as *;
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,18 @@
 export default{
     data () {
         return{
-
+            list: [
+                "caracters",
+                "comics",
+                "movies",
+                "tv",
+                "games",
+                "collectibles",
+                "videos",
+                "fans",
+                "news",
+                "shop"
+            ]
         }
     }
 }
@@ -10,8 +21,53 @@ export default{
 
 <template>
     <header>
-        <h1>Header</h1>
+        <div id="header-section" class="container">
+            <img src="../assets/img/dc-logo.png" alt="logo-DC">
+            <div id="list">
+                <ul v-for="item in list">
+                    <li>
+                        {{ item }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+  header{
+    height: 80px;
+
+    #header-section{
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        text-transform: uppercase;
+
+        img{
+         width: 5%;
+        }
+
+        #list{
+            display: flex;
+
+            ul{
+            display: flex;
+
+                 li{
+                    font-size: 12px;
+                    margin: 0.5rem;
+
+                    &:hover{
+                        color: blue;
+                        cursor: pointer;
+                    }
+                 }
+            }
+
+        }
+    }
+  }
+ 
+</style>
